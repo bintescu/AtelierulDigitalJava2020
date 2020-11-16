c292807337ff6505185506a37592f7fb7497db49
bintescu <41958601+bintescu@users.noreply.github.com>
2020-11-16 20:46:25 +0200

    create first repository

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] myArr;
+        myArr = new int[4];
+        myArr[0] = 100;
+        myArr[1] = 200;
+        myArr[2] = 300;
+        for(int i = 0 ; i < myArr.length ; i ++ ){
+            System.out.println(myArr[i]);
+        }
+    }
+}
